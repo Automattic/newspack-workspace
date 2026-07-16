4ad2665e796b10e51babe764b627667548daa5c7
Adam Cassis <adam.cassis@automattic.com>
2026-07-16 12:35:14 +0200

    chore: shepherd --provision smoke test (throwaway, do not merge)

---
+++ plugins/newspack-plugin/includes/class-shepherd-provision-smoke.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Throwaway file for a shepherd --provision smoke test. Not loaded anywhere.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Smoke test class.
+ */
+class Shepherd_Provision_Smoke {
+	/**
+	 * Say hello.
+	 *
+	 * @param string $name A name.
+	 * @return string
+	 */
+	public static function hello( $name ) {
+		if ($name) {
+			return 'hello ' . $name;
+		}
+		return 'hello';
+	}
+}
