61eeac9bee48b6524f93dcd9464d7c1af456dbcb
Adam Cassis <adam.cassis@automattic.com>
2026-08-19 15:36:35 +0200

    feat(cli): add fix-memberships command (NPPM-386, #914)

--- plugins/newspack-plugin/includes/cli/class-initializer.php
+++ plugins/newspack-plugin/includes/cli/class-initializer.php
@@ -35,6 +35,7 @@ class Initializer {
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-teams-migration.php';
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-institutions-migration.php';
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-membership-gates-migration.php';
+		include_once NEWSPACK_ABSPATH . 'includes/cli/class-fix-memberships.php';
 	}
 
 	/**
@@ -129,6 +130,14 @@ class Initializer {
 				);
 			}
 			WP_CLI::add_command( 'newspack migrate-membership-gates', [ 'Newspack\CLI\Membership_Gates_Migration', 'migrate_membership_gates' ] );
+		}
+
+		// Only register the fix-memberships command when WC Memberships is active.
+		if ( function_exists( 'wc_memberships' ) ) {
+			WP_CLI::add_command(
+				'newspack fix-memberships',
+				[ 'Newspack\CLI\Fix_Memberships', 'run' ]
+			);
 		}
 
 		Optional_Modules::register_commands();
