31d93e8382b797d3b9af90c9eda0dba0e1380c3c
Miguel Peixe <miguel.peixe@automattic.com>
2026-08-21 12:37:37 -0300

    style(audience): prettier formatting on scope-picker files

--- plugins/newspack-plugin/src/wizards/audience/views/pricing-rules/scope-targets.test.js
+++ plugins/newspack-plugin/src/wizards/audience/views/pricing-rules/scope-targets.test.js
@@ -13,24 +13,16 @@ import { SOURCES } from './scope-targets';
 
 describe( 'scope-targets SOURCES', () => {
 	it( 'products search uses the engine route, which serves variations', () => {
-		expect( SOURCES.product_ids.suggestionsPath( 'gold' ) ).toBe(
-			'/wc-dynamic-pricing/v1/products?search=gold&per_page=20'
-		);
+		expect( SOURCES.product_ids.suggestionsPath( 'gold' ) ).toBe( '/wc-dynamic-pricing/v1/products?search=gold&per_page=20' );
 	} );
 
 	it( 'saved product ids hydrate through the engine route include param', () => {
-		expect( SOURCES.product_ids.savedPath( [ 4201, 42 ] ) ).toBe(
-			'/wc-dynamic-pricing/v1/products?include=4201%2C42'
-		);
+		expect( SOURCES.product_ids.savedPath( [ 4201, 42 ] ) ).toBe( '/wc-dynamic-pricing/v1/products?include=4201%2C42' );
 	} );
 
 	it( 'categories stay on core WP REST', () => {
-		expect( SOURCES.category.suggestionsPath( 'news' ) ).toBe(
-			'/wp/v2/product_cat?search=news&per_page=20&_fields=id%2Cname'
-		);
-		expect( SOURCES.category.savedPath( [ 1, 2, 3 ] ) ).toBe(
-			'/wp/v2/product_cat?include=1%2C2%2C3&per_page=3&_fields=id%2Cname'
-		);
+		expect( SOURCES.category.suggestionsPath( 'news' ) ).toBe( '/wp/v2/product_cat?search=news&per_page=20&_fields=id%2Cname' );
+		expect( SOURCES.category.savedPath( [ 1, 2, 3 ] ) ).toBe( '/wp/v2/product_cat?include=1%2C2%2C3&per_page=3&_fields=id%2Cname' );
 	} );
 
 	it( 'only id-scoped types have a source', () => {
